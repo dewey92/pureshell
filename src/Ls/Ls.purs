f41729b5a1847c0cc3b9e0935807d0466a63b727
Jihad D. Waspada <j.waspada@youngcapital.nl>
2019-07-21 21:34:49 +0200

    implement -la

--- src/Ls/Ls.purs
+++ src/Ls/Ls.purs
@@ -1,10 +1,13 @@
-module PureShell.Ls.Ls where
+module PureShell.Ls.Ls
+  ( LsOptions
+  , ls
+  ) where
 
 import Prelude
 
 import Data.Array (filter, last)
 import Data.Bifunctor (bimap)
-import Data.Either (Either(..), either, isRight)
+import Data.Either (Either(..), either)
 import Data.Function.Uncurried (runFn0)
 import Data.Maybe (maybe)
 import Data.String (Pattern(..), joinWith, split)
@@ -16,6 +19,7 @@ import Effect.Console (log, logShow)
 import Node.FS.Aff as FS
 import Node.FS.Stats (Stats(..))
 import Node.Path (FilePath)
+import Options.Applicative.Internal.Utils (startsWith)
 import PureShell.Common.Utility (logEscape)
 
 data LsError = FileOrDirNotExists
@@ -33,7 +37,11 @@ type LsOptions = {
   withTrailingSlash :: Boolean
 }
 
--- | TODO: Give better naming to it
+-- | `TODO: A Reader Monad is probably a good idea to be used in this module since mostly
+-- | these functions rely on `FilePath` and `LsOption` value
+-- | type LsReader r = Reader (FilePath /\ LsOptions) r
+
+-- | TODO: Give better name
 type ErrorOrFileStats = (Either LsError (FilePath /\ Stats))
 
 ls :: FilePath -> LsOptions -> Aff Unit
@@ -43,21 +51,26 @@ ls filePath options = do
     Left e -> liftEffect $ logShow e
     Right r@(_ /\ Stats s) ->
       if runFn0 s.isFile
-      then showFileStats r
-      else showDirStats filePath
+      then showFileStats r options
+      else showDirStats filePath options
 
 -- | When the given input is a file, yeild the stat immediately
-showFileStats :: (FilePath /\ Stats) -> Aff Unit
-showFileStats = formatStats >>> log >>> liftEffect
+showFileStats :: (FilePath /\ Stats) -> LsOptions -> Aff Unit
+showFileStats stats options = formatStats stats options # log # liftEffect
 
 -- | When the given input is a directory, list that directory with the stats
 -- | then join the result with a breakline
-showDirStats :: FilePath -> Aff Unit
-showDirStats filePath = getDirs filePath >>= concludeStats >>> logEscape >>> liftEffect
+showDirStats :: FilePath -> LsOptions -> Aff Unit
+showDirStats filePath options = getDirs filePath >>= concludeStats >>> logEscape >>> liftEffect
   where
     concludeStats :: Array ErrorOrFileStats -> String
-    concludeStats = filter isRight
-      >>> map (either (const "oops") formatStats)
+    concludeStats = filter (case _ of
+        Left _ -> false
+        Right (fp /\ (Stats s)) ->
+          -- | Exclude hidden files (starts with `.`) when `options.withHiddelFiles` is false
+          options.withHiddenFiles || not (startsWith (Pattern ".") $ removePath fp)
+      )
+      >>> map (either (const "oops") (flip formatStats options))
       >>> joinWith "\n"
 
 getStat :: FilePath -> Aff ErrorOrFileStats
@@ -70,14 +83,18 @@ getDirs dir = FS.readdir dir >>= traverse (appendDirPrefix >>> getStat)
   where
     appendDirPrefix content = dir <> "/" <> content
 
-formatStats :: (FilePath /\ Stats) -> String
-formatStats (filePath /\ (Stats stats)) =
-  show stats.mode <> " " <>
-  show stats.nlink <> " " <>
-  show stats.uid <> " " <>
-  show stats.gid <> " " <>
-  show stats.size <> " " <>
-  show stats.mtime <> " " <>
-  maybe filePath identity trimmedDirName
+formatStats :: (FilePath /\ Stats) -> LsOptions -> String
+formatStats (filePath /\ (Stats stats)) options = withStats <> (removePath filePath)
   where
-    trimmedDirName = split (Pattern "/") filePath # last
+    withStats :: String
+    withStats = if not options.withStats
+      then ""
+      else show stats.mode <> " " <>
+        show stats.nlink <> " " <>
+        show stats.uid <> " " <>
+        show stats.gid <> " " <>
+        show stats.size <> " " <>
+        show stats.mtime <> " "
+
+removePath :: FilePath -> FilePath
+removePath fp = split (Pattern "/") fp # last # maybe fp identity
