20b369477dd1cf6e15e6e1ba015bfeb18226a6d3
Jihad Dzikri Waspada <dewey992@gmail.com>
2019-07-22 04:17:44 +0200

    delete unused lines

--- src/Common/FileM.purs
+++ src/Common/FileM.purs
@@ -26,25 +26,3 @@ class MonadError e m <= MonadFs e m | m -> e where
   readTextFile :: Encoding -> FilePath -> m String
   readDir :: FilePath -> m (List FilePath)
   getStat :: FilePath -> m Stats
-
--- | To be a monad, a type must implement the `Functor`, `Apply`, `Applicative`,
--- | and `Bind` type classes. Please implement also `MonadEffect` and `MonadAFf`
--- | later on to be able to run under `Effect` adn `Aff` monad.
--- |
--- | ```purescript
--- |    newtype AppM e a = AppM (Either e a)
---
--- |    derive newtype instance functorApp :: Functor (AppM e)
--- |    derive newtype instance applyApp :: Apply (AppM e)
--- |    derive newtype instance applicativeApp :: Applicative (AppM e)
--- |    derive newtype instance bindApp :: Bind (AppM e)
--- |    derive newtype instance monadApp :: Monad (AppM e)
--- |    derive newtype instance monadThrowApp :: MonadThrow e (AppM e)
--- |    derive newtype instance monadErrorApp :: MonadError e (AppM e)
--- |
--- |    instance appFs :: MonadFs e (AppM e) where
--- |      exists _ = pure true
--- |      readTextFile _ _ = pure ""
--- |      readDir _ = pure ("" : Nil)
--- |      getStat _ = return
--- | ```
