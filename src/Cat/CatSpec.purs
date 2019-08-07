32206db61986eea96e0b584985566f1e36a4098e
dewey92 <dewey992@gmail.com>
2019-08-07 03:02:11 +0200

    try new test approach

--- src/Cat/CatSpec.purs
+++ src/Cat/CatSpec.purs
@@ -3,17 +3,13 @@ module PureShell.Cat.CatSpec (spec) where
 import Prelude
 
 import Control.Monad.Error.Class (throwError)
-import Control.Monad.Except (ExceptT(..))
 import Data.Either (Either(..))
-import Data.List (List(..))
-import Effect.Aff (Aff, Error, error)
 import PureShell.Cat.Cat (CatErrors(..), cat)
-import PureShell.Common.MonadFS (class MonadFS)
-import PureShell.Mock.MonadFS (mockMetadata)
+import PureShell.Mock.MonadFS (MonadFSIns(..), baseInst, runTestM)
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 
-newtype TestCatM a = TestCatM (Aff a)
+{- newtype TestCatM a = TestCatM (Aff a)
 
 runTest :: ∀ e r. ExceptT e TestCatM r -> Aff (Either e r)
 runTest (ExceptT (TestCatM a)) = a
@@ -33,20 +29,29 @@ instance monadFSTestCatM :: MonadFS Error TestCatM where
     | otherwise = throwError $ error "a dir"
   readDir _ = pure Nil
   getMetadata _ = pure mockMetadata
-  rename _ _ = pure unit
+  rename _ _ = pure unit -}
 
 spec :: Spec Unit
 spec = describe "cat" do
   it "throws when reading an invalid path" do
     let invalidPath = "invalid/path"
-    err <- runTest $ cat invalidPath
+    let inst = MonadFSIns $ baseInst { _exists = \_ -> pure false }
+    err <- runTestM inst $ cat invalidPath
     err `shouldEqual` (Left $ FileNotExists invalidPath)
 
   it "throws when a directory is passed" do
     let dirPath = "someDir"
-    err <- runTest $ cat dirPath
+    let inst = MonadFSIns $ baseInst {
+      _exists = \_ -> pure true,
+      _readFile = \_ -> throwError $ MiscError dirPath
+    }
+    err <- runTestM inst $ cat dirPath
     err `shouldEqual` (Left $ MiscError dirPath)
 
   it "succesfully reads the file" do
-    res <- runTest $ cat "validFile"
+    let inst = MonadFSIns $ baseInst {
+      _exists = \_ -> pure true,
+      _readFile = \_ -> pure "file content"
+    }
+    res <- runTestM inst $ cat "validFile"
     res `shouldEqual` (Right $ "file content")
