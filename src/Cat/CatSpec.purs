128437fbf52ff88aa7ef4202a3ad9aab00d20032
dewey92 <dewey992@gmail.com>
2019-08-07 03:03:48 +0200

    remove unnec line

--- src/Cat/CatSpec.purs
+++ src/Cat/CatSpec.purs
@@ -9,28 +9,6 @@ import PureShell.Mock.MonadFS (MonadFSIns(..), baseInst, runTestM)
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 
-{- newtype TestCatM a = TestCatM (Aff a)
-
-runTest :: ∀ e r. ExceptT e TestCatM r -> Aff (Either e r)
-runTest (ExceptT (TestCatM a)) = a
-
-derive newtype instance functorTestCatM :: Functor TestCatM
-derive newtype instance applyTestCatM :: Apply TestCatM
-derive newtype instance applicativeTestCatM :: Applicative TestCatM
-derive newtype instance bindTestCatM :: Bind TestCatM
-derive newtype instance monadTestCatM :: Monad TestCatM
-
-instance monadFSTestCatM :: MonadFS Error TestCatM where
-  exists fp
-    | fp == "validFile" || fp == "someDir" = pure true
-    | otherwise = pure false
-  readFile fp
-    | fp == "validFile" = pure "file content"
-    | otherwise = throwError $ error "a dir"
-  readDir _ = pure Nil
-  getMetadata _ = pure mockMetadata
-  rename _ _ = pure unit -}
-
 spec :: Spec Unit
 spec = describe "cat" do
   it "throws when reading an invalid path" do
